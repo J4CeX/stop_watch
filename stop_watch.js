6fa7da0aca789ec16c2cdecf2d4175ee06cb427c
Jacek <jacek_kozakowski_dev@wp.pl>
2023-11-02 20:02:15 +0100

    Version 3

--- stop_watch.js
+++ stop_watch.js
@@ -1,10 +1,10 @@
 let isPlaying = false;
-let seconds = 0;
-let minutes = 0;
-let hours = 0;
+let seconds = JSON.parse(localStorage.getItem('seconds'));
+let minutes = JSON.parse(localStorage.getItem('minutes'));;
+let hours = JSON.parse(localStorage.getItem('hours'));;
 
 let intervalId;
-let time = '';
+let time = ``;
 
 const startButton = document.getElementById('start');
 const stopButton = document.getElementById('stop');
@@ -12,8 +12,8 @@ const resetButton = document.getElementById('reset');
 
 startButton.addEventListener('click', () => {
   startButton.classList.toggle('hidden');
-  stopButton.classList.toggle('hidden');
-  resetButton.classList.toggle('hidden');
+  stopButton.classList.remove('hidden');
+  resetButton.classList.remove('hidden');
   
   if(!isPlaying) {
     isPlaying = true;
@@ -26,17 +26,20 @@ startButton.addEventListener('click', () => {
 });
 
 stopButton.addEventListener('click', () => {
+  if(seconds == 0 && minutes == 0 && hours == 0) {
+    resetButton.classList.toggle('hidden');
+  }
   startButton.classList.toggle('hidden');
   stopButton.classList.toggle('hidden');
-  resetButton.classList.toggle('hidden');
+  
 
   isPlaying = false;
   clearInterval(intervalId);
 });
 
 resetButton.addEventListener('click', () => {
-  startButton.classList.toggle('hidden');
-  stopButton.classList.toggle('hidden');
+  startButton.classList.remove('hidden');
+  stopButton.classList.add('hidden');
   resetButton.classList.toggle('hidden');
 
   isPlaying = false;
@@ -84,5 +87,10 @@ const displayTime = () => {
     time += '00';
   }
   const watch = document.getElementById('watch');
-watch.innerHTML = time;
+  watch.innerHTML = time;
+  localStorage.setItem('seconds', JSON.stringify(seconds));
+  localStorage.setItem('minutes', JSON.stringify(minutes));
+  localStorage.setItem('hours', JSON.stringify(hours));
 }
+
+displayTime();
